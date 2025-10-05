9705410dfda2487fd8baa5a38e13b144d7bb4ba1
evftrya <evfi29@gmail.com>
2025-10-05 21:43:58 +0700

    list pegawai front end

--- resources/views/kelola_data/parts/sidebar.blade.php
+++ resources/views/kelola_data/parts/sidebar.blade.php
@@ -3,62 +3,56 @@
 
 @php
 $sidebars = [
-    ['Manajemen Akun',
-        [
-            ['Daftar Akun',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Akun',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
-        ]
-    ],
     ['Manajemen Data Pegawai',
         [
-            ['Daftar Pegawai',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Pegawai Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
-            ['Import Pegawai',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
-            ['Dashboard Pegawai',  route('manage.account.dashboard'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Pegawai',  'manage.pegawai.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Pegawai Baru',  'manage.pegawai.new', 'fa-slab fa-regular fa-inbox'],
+            ['Import Pegawai',  'manage.pegawai.new', 'fa-slab fa-regular fa-inbox'],
+            ['Dashboard Pegawai',  'manage.pegawai.dashboard', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Data Dosen',
         [
-            ['Daftar Dosen',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Dosen Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
-            ['Import Dosen',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Dosen',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Dosen Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
+            ['Import Dosen',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Fakultas',
         [
-            ['Daftar Fakultas',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Fakultas Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Fakultas',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Fakultas Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Program Studi',
         [
-            ['Daftar Program Studi',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Program Studi Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Program Studi',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Program Studi Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Level',
         [
-            ['Daftar Level',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Level Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Level',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Level Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Pengawakan',
         [
-            ['Daftar Pengawakan',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Pengawakan Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Pengawakan',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Pengawakan Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen Formasi',
         [
-            ['Daftar Formasi',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah Formasi Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar Formasi',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah Formasi Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
     ['Managemen COE',
         [
-            ['Daftar COE',  route('manage.account.list'), 'fa-slab fa-regular fa-inbox'],
-            ['Tambah COE Baru',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
-            ['Import COE',  route('manage.account.new'), 'fa-slab fa-regular fa-inbox'],
+            ['Daftar COE',  'manage.account.list', 'fa-slab fa-regular fa-inbox'],
+            ['Tambah COE Baru',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
+            ['Import COE',  'manage.account.new', 'fa-slab fa-regular fa-inbox'],
         ]
     ],
 
@@ -68,7 +62,7 @@ $sidebars = [
 @foreach ($sidebars as  $sidebar)
 <x-sidebar-group title="{{$sidebar[0]}}" icon="fa-users">
     @foreach ($sidebar[1] as $button) 
-    <x-sidebar-button href="/{{$button[1]}}" icon="{{$button[2]}}" label="{{$button[0]}}" />
+    <x-sidebar-button href="{{route($button[1])}}" icon="{{$button[2]}}" label="{{$button[0]}}" />
     @endforeach
 </x-sidebar-group>
 @endforeach
