4c1edc05cfbe72ff81cb9028cedec95c2f8ebc39
Yoga Wilanda <hyoga.wilanda@gmail.com>
2025-10-14 20:25:14 +0700

    fix: bug fix and add logo for the FE

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,6 @@
-<nav x-data="{ open: false }" class="bg-blue-950 border-b border-gray-100 dark:bg-gray-800 dark:border-gray-700">
+<nav x-data="{ open: false }" class="border-b border-gray-100 bg-blue-950 dark:bg-gray-800 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    {{-- <div class="px-4 mx-2 max-w-7xl sm:px-6 lg:px-8"> --}}
-    <div class="px-4 mx-3 max-w-full sm:px-6 lg:px-8">
+    <div class="max-w-full px-4 mx-3 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <!-- Left: Logo -->
             <div class="flex items-center">
@@ -14,7 +13,7 @@
 
             <!-- Center: Navigation Links -->
             <div class="hidden sm:flex sm:items-center sm:justify-center sm:flex-1">
-                <div class="space-x-8 sm:flex text-white">
+                <div class="space-x-8 text-white sm:flex">
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         Beranda
                     </x-nav-link>
@@ -50,12 +49,12 @@
                                     <div>{{ Auth::user()->name }}</div>
 
                                     <div class="ms-1">
-                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                        <!-- <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 20 20">
                                             <path fill-rule="evenodd"
                                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                                 clip-rule="evenodd" />
-                                        </svg>
+                                        </svg> -->
                                     </div>
                                 </button>
                             </x-slot>
@@ -84,7 +83,7 @@
                         </x-dropdown>
                     </div>
                 @else
-                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <div class="hidden text-white sm:flex sm:items-center sm:ms-6">
                         <x-nav-link :href="route('login')">
                             {{ __('Login') }}
                         </x-nav-link>
@@ -95,14 +94,14 @@
                 <div class="flex items-center -me-2 sm:hidden">
                     <button @click="open = ! open"
                         class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400">
-                        <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <!-- <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                                 stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M4 6h16M4 12h16M4 18h16" />
                             <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden"
                                 stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        </svg> -->
                     </button>
                 </div>
             </div>
@@ -113,7 +112,7 @@
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1 text-white">
             <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                <p class="text-white clear-p">Beranda</p>
+                <p class="text-white ">Berandaaaa</p>
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="url('/presensi')" :active="request()->is('presensi*')">
                 {{-- Presensi --}}
@@ -184,7 +183,7 @@
                     </div>
                 </div>
             @else
-                <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                <div class="pt-4 pb-1 text-white border-t border-gray-200 dark:border-gray-600">
                     <div class="px-4">
                         <x-responsive-nav-link :href="route('login')">
                             {{ __('Login') }}
